4f5f7de091d41aec81ed9ee8b3df02856f07b735
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-03 00:32:16 +0430

    add js file and basic function and varibles

---
+++ script.js
@@ -0,0 +1,24 @@
+function able() {
+  var text = [
+    "Animi Dolor Pariatur",
+    "Art Deco",
+    "Artificial potted plant",
+    "Dark Green Jug",
+    "Drinking Glesses",
+    "Helen Chair",
+    "High Quality Glass Bottle",
+    "Living Room & Lights",
+    "Nancy Chair",
+    "Simple Chair",
+    "Smooth Disk",
+    "Table Black",
+    "Table Wood Pine",
+    "Teapot with black tea",
+    "Unique Decoration",
+    "Vase Of Flowers",
+    "Wood Eggs",
+    "Wooden Box",
+    "Wooden Cups",
+  ];
+  let row = document.querySelector("#box-2");
+}
